26fa7af7cbbcef11442076784655bf2d4a1df2d2
Mat Sletten <matsletten@reception-desk.bbbb.net>
2015-01-30 11:26:44 -0800

    Updates

--- Web Content/StringPlayground.playground/section-1.swift
+++ Web Content/StringPlayground.playground/section-1.swift
@@ -3,3 +3,31 @@
 import UIKit
 
 var str = "Hello, playground"
+var str2 = "Mat"
+var str3 = str + str2
+//This is a concatenation of strings
+for character in str {
+    println(character)
+}
+//There are two types of strings in Swift: String and NSString (obj-c)
+var str4 = "Test String" as NSString
+
+var subString = str4.substringToIndex(6)
+//this only works with NSString type, and takes the index up to the int value
+
+var subString2 = (str as NSString).substringToIndex(8)
+//we can downcast using parens here to reType a String to NSString
+
+var endingString = str4.substringFromIndex(3)
+//this substrings starting from the index int
+
+var endString = str4.substringWithRange(NSRange(location: 5, length: 2))
+
+str4.containsString("Mat")
+str4.containsString("Test")
+
+
+str4.componentsSeparatedByString(" ")
+//can specify here if the components are separated by a string or a character
+str4.uppercaseString
+str4.lowercaseString
--- Web Content/ViewController.swift
+++ Web Content/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
                             
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var url = NSURL(string: "http://www.stackoverflow.com")
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url)
+        {
+            (data, response, error) in println(NSString(data: data, encoding:NSUTF8StringEncoding))
+            //using the above to log to console, we can retrieve the html for any website and view it
+            
+        }
+        task.resume()
+        //it's standard to use task as a variable/let name for a URL Session
     }
 
     override func didReceiveMemoryWarning() {
